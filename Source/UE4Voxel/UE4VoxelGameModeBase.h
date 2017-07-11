945f3a8a062a11abc139f659f9d7e0d4ac256584
TheGoodDoktor <mrmarkcraig@googlemail.com>
2017-07-11 11:28:14 +0100

    Initial commit

---
+++ Source/UE4Voxel/UE4VoxelGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE4VoxelGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE4VOXEL_API AUE4VoxelGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
